0303c51a797e26e3a261d023883f68ac98018760
Guto Hertzog <guto.hertzog@gmail.com>
2023-04-26 18:02:50 -0300

    aula 15; parte exercícios 05

---
+++ exercicios/aula05.py
@@ -0,0 +1,99 @@
+import math
+# 1. Crie um programa que peça ao usuário para digitar seu nome e, em seguida, imprima "Olá, [nome do usuário]!".
+nome = input('Digite seu nome: ')
+print(f'Olá, {nome}!')
+# 2. Crie um programa que peça ao usuário para digitar sua idade e, em seguida, imprima "Você nasceu em [ano de nascimento]".
+idade = int(input('Digite sua idade: '))
+print(f'Você nasceu em {2021 - idade}')
+# 3. Crie um programa que peça ao usuário para digitar um número e, em seguida, imprima o dobro desse número.
+numero = int(input('Digite um número: '))
+print(f'O dobro de {numero} é {numero * 2}')
+# 4. Crie um programa que peça ao usuário para digitar dois números e, em seguida, imprima a soma desses números.
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+print(f'A soma de {numero1} e {numero2} é {numero1 + numero2}')
+# 5. Crie um programa que peça ao usuário para digitar um número e, em seguida, imprima o valor absoluto desse número.
+numero = int(input('Digite um número: '))
+print(f'O valor absoluto de {numero} é {abs(numero)}')
+# 6. Crie um programa que peça ao usuário para digitar uma temperatura em graus Celsius e, em seguida, imprima a temperatura em graus Fahrenheit.
+celsius = float(input('Digite a temperatura em graus Celsius: '))
+print(f'A temperatura em graus Fahrenheit é {celsius * 1.8 + 32}')
+# 7. Crie um programa que peça ao usuário para digitar sua altura em metros e, em seguida, imprima sua altura em centímetros.
+altura = float(input('Digite sua altura em metros: '))
+print(f'Sua altura em centímetros é {altura * 100}')
+# 8. Crie um programa que peça ao usuário para digitar seu peso em quilogramas e, em seguida, imprima seu peso em libras.
+peso = float(input('Digite seu peso em quilogramas: '))
+print(f'Seu peso em libras é {peso * 2.20462}')
+# 9. Crie um programa que peça ao usuário para digitar um número e, em seguida, imprima a raiz quadrada desse número.
+numero = int(input('Digite um número: '))
+print(f'A raiz quadrada de {numero} é {numero ** 0.5}')
+# 10.  Crie um programa que peça ao usuário para digitar um número e, em seguida, imprima o seu logaritmo na base 10.
+numero = int(input('Digite um número: '))
+print(f'O logaritmo de {numero} na base 10 é {math.log10(numero)}')
+# 11.  Crie um programa que peça ao usuário para digitar seu nome e idade, e em seguida, imprima "Olá, [nome do usuário], você tem [idade] anos!".
+# 12.  Crie um programa que peça ao usuário para digitar um número e, em seguida, imprima uma contagem de ocorrência cada número individualmente.
+# 13.  Receba uma string do usuário e exiba-a em letras maiúsculas com a primeira letra em minúscula.
+# 14.  Receba uma string do usuário e substitua todas as letras "a" por "o".
+# 15.  Receba uma string do usuário e conte quantas vezes a letra "e" aparece.
+# 16.  Receba uma string do usuário e conte quantas palavras existem nela.
+# 17.  Receba uma string do usuário e remova todos os espaços em branco.
+# 18.  Receba uma string do usuário e remova todos os caracteres que não são letras.
+# 19.  Receba uma string do usuário e troque todas as vogais por "*".
+# 20.  Receba uma string do usuário e conte quantas vezes a palavra "python" aparece.
+# 21.  Receba duas strings do usuário e exiba-as concatenadas.
+# 22.  Receba uma string do usuário e exiba-a em letras maiúsculas com a última letra em minúscula.
+# 23.  Receba uma string do usuário e troque todas as letras "s" por "$".
+# 24.  Receba uma string do usuário e conte quantas vezes a palavra "exemplo" aparece.
+# 25.  Receba uma string do usuário e substitua todas as letras "o" por "a".
+# 26.  Receba uma string do usuário e remova todos os caracteres que não são letras nem números.
+# 27.  Receba uma string do usuário e exiba-a em letras minúsculas com a primeira letra em maiúscula.
+# 28.  Receba uma string do usuário e remova todas as letras "r".
+# 29.  Receba uma string do usuário e exiba-a em letras maiúsculas com todas as vogais em minúsculas.
+# 30.  Receba uma string do usuário e conte quantas vezes a letra "a" aparece.
+# 31.  Peça ao usuário para inserir um número inteiro e uma palavra. Repita a palavra o número de vezes indicado pelo usuário.
+# 32.  Peça ao usuário para inserir um número e uma palavra. Imprima a palavra concatenada com o número sem espaço.
+# 33.  Peça ao usuário para inserir uma frase longa. Imprima a frase com todas as vogais substituídas por asteriscos.
+# 34.  Receba um número do usuário e calcule a raiz quadrada dele, arredondando o resultado para duas casas decimais.
+# 35.  Receba dois números do usuário e calcule a potência do primeiro elevado ao segundo, arredondando o resultado para duas casas decimais.
+# 36.  Receba um número do usuário e calcule o logaritmo natural desse número, arredondando o resultado para duas casas decimais.
+# 37.  Receba um número do usuário e calcule o seu valor absoluto.
+# 38.  Receba dois números do usuário e calcule o quociente e o resto da divisão do primeiro pelo segundo, respectivamente.
+# 39.  Receba um número do usuário e calcule o seu fatorial.
+# 40.  Receba um número do usuário e calcule o seu arredondamento para baixo e para cima, respectivamente.
+# 41.  Receba dois números do usuário e calcule a potência do primeiro elevado ao segundo, arredondando o resultado para baixo e para cima, respectivamente.
+# 42.  Receba um número do usuário e calcule o seu valor absoluto e a sua raiz quadrada, respectivamente.
+# 43.  Receba dois números do usuário e calcule o logaritmo natural do primeiro elevado ao segundo, arredondando o resultado para duas casas decimais.
+# 44.  Receba dois números do usuário e calcule o valor absoluto da diferença entre eles.
+# 45.  Receba um número do usuário e calcule a sua raiz quadrada, arredondando o resultado para baixo e para cima, respectivamente.
+# 46.  Receba um número do usuário e calcule o seu arredondamento para baixo e para cima, respectivamente. Em seguida, calcule a potência de 2 elevado a cada um desses valores arredondados.
+# 47.  Receba um número do usuário e calcule o logaritmo natural desse número, arredondando o resultado para baixo e para cima, respectivamente. Em seguida, calcule a potência de 2 elevado a cada um desses valores arredondados.
+# 48.  Receba dois números do usuário e calcule o logaritmo natural do primeiro elevado ao segundo, arredondando o resultado para baixo e para cima, respectivamente. Em seguida, calcule a potência de 2 elevado a cada um desses valores arredondados.
+# 49.  Receba dois números do usuário e calcule o quociente e o resto da divisão do primeiro pelo segundo, respectivamente. Em seguida, calcule a raiz quadrada de cada um desses valores e some os resultados.
+# 50.  Receba um número do usuário e calcule o seu fatorial. Em seguida, calcule o logaritmo natural desse valor, arredondando o resultado para baixo e para cima, respectivamente.
+# 51.  Receba um número do usuário e calcule a sua raiz quadrada. Em seguida, calcule o valor absoluto da diferença entre esse valor e o valor da constante pi do módulo math.
+# 52.  Receba dois números do usuário e calcule o arredondamento para baixo e para cima do primeiro, respectivamente. Em seguida, calcule a potência do segundo elevado a cada um desses valores arredondados e some os resultados.
+# 53.  Receba um número do usuário e calcule a sua raiz quadrada. Em seguida, calcule o valor absoluto da diferença entre esse valor e a constante de Euler do módulo math.
+# 54.  Receba dois números do usuário e calcule a potência do primeiro elevado ao segundo. Em seguida, calcule o valor absoluto da diferença entre esse resultado e a constante de Euler do módulo math.
+# 55.  Receba um número do usuário e calcule o logaritmo natural desse número. Em seguida, calcule a raiz quadrada desse valor e multiplique pelo valor de pi do módulo math.
+# 56.  Receba dois números do usuário e calcule o quociente e o resto da divisão do primeiro pelo segundo, respectivamente. Em seguida, calcule a potência de 2 elevado ao quociente e multiplique pelo resto.
+# 57.  Receba um número do usuário e calcule a sua raiz quadrada. Em seguida, calcule o seu arredondamento para baixo e para cima, respectivamente. Em seguida, calcule o fatorial de cada um desses valores arredondados e some os resultados.
+# 58.  Receba do usuário o valor de um ângulo em graus e calcule o seno desse ângulo utilizando a função sin do módulo math.
+# 59.  Receba do usuário dois números inteiros e calcule o resultado da operação de divisão inteira utilizando a função divmod do módulo math.
+# 60.  Receba do usuário um número float e calcule a sua parte inteira utilizando a função floor do módulo math.
+# 61.  Receba do usuário um número inteiro e calcule o seu fatorial utilizando a função factorial do módulo math.
+# 62.  Receba do usuário um número float e calcule o seu valor absoluto utilizando a função abs do módulo math.
+# 63.  Receba do usuário um número float e calcule a sua raiz quadrada utilizando a função sqrt do módulo math.
+# 64.  Receba do usuário o valor do raio de um círculo e calcule a sua área utilizando a constante pi do módulo math.
+# 65.  Receba do usuário o valor de uma base e de um expoente e calcule a potência utilizando a função pow do módulo math.
+# 66.  Receba do usuário um número float e arredonde-o para cima utilizando a função ceil do módulo math.
+# 67.  Receba do usuário um número float e arredonde-o para baixo utilizando a função floor do módulo math.
+# 68.  Receba do usuário dois números inteiros e calcule o máximo divisor comum utilizando a função gcd do módulo math.
+# 69.  Receba do usuário o valor de um ângulo em graus e calcule o seu cosseno utilizando a função cos do módulo math.
+# 70.  Receba do usuário um número float e calcule a sua parte decimal utilizando a função modf do módulo math.
+# 71.  Receba do usuário dois números float e calcule o seu mínimo utilizando a função fmin do módulo math.
+# 72.  Receba do usuário um número float e calcule o seu logaritmo natural utilizando a função log do módulo math.
+# 73.  Receba do usuário dois números inteiros e calcule o mínimo múltiplo comum utilizando a função lcm do módulo math.
+# 74.  Receba do usuário o valor de um ângulo em radianos e calcule a sua tangente utilizando a função tan do módulo math.
+# 75.  Receba do usuário um número float e calcule o seu arco tangente utilizando a função atan do módulo math.
+# 76.  Receba do usuário dois números inteiros e calcule a potência modular utilizando a função pow do módulo math.
+# 77.  Receba do usuário um número float e calcule a sua exponencial utilizando a função exp do módulo math.
